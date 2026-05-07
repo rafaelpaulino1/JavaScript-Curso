12a210ee4e44b9fe19aaa13be0b4f6f803dfdf95
Rafael Paulino de Andrade <rafaelpaulino.tsl@gmail.com>
2026-05-07 01:00:24 -0300

    Finalização da Aula do Dia 02
    Está sendo upado o código js com alguns exemplos mais as anotações organizadas feitas durante a aula.

--- dia02/index.js
+++ dia02/index.js
@@ -1,3 +1,27 @@
 console.log("Dia 02");
 
-//Variáveis: são usadas para armazenar dados em JavaScript
+let address = "Brasília";
+
+console.log(address);
+
+address = "USA";
+
+console.log(address);
+
+let age = 12;
+
+age = 52;
+
+let nome, salario, departamento;
+
+salario = null;
+
+let estudante ={
+    nome: "Eduarda",
+    idade: 24,
+    matriculado: true
+};
+
+console.log(estudante.nome);
+
+let arr = [1,2,3,4,5];
