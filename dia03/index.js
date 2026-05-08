e0106e3f830e91c77ca34ec9be8b2246247561ad
Rafael Paulino de Andrade <rafaelpaulino.tsl@gmail.com>
2026-05-08 20:34:16 -0300

    Dia 03 parte 1
    estudo ate operadores de comparação

---
+++ dia03/index.js
@@ -0,0 +1,71 @@
+console.log("Dia 03");
+
+console.log("--OPERADORES ARITMÉTICOS--");
+
+let a=10;
+let b=20;
+
+let dontPanic=42;
+let p_nome="Rafael";
+let sobrenome="Paulino";
+
+console.log(a+b);
+console.log(a-b);
+console.log(b-a);
+console.log(a*b);
+console.log(a/b);
+console.log(b/a);
+
+console.log("Rafa"+dontPanic); // VALOR NUMÉRICO COM UMA STRING
+console.log(p_nome+sobrenome); // STRING COM STRING
+
+let c = 5;
+let d = 2;
+
+console.log(c**d);
+console.log(c%d);
+
+let contador=5;
+let contador2=5;
+
+//CONTADORES
+console.log(contador++);
+console.log(contador);
+console.log(++contador);
+
+console.log(contador2--);
+console.log(contador2);
+console.log(--contador2);
+
+console.log("--OPERADORES DE ATRIBUIÇÃO--");
+
+let x = 10;
+
+console.log(x+=5);
+console.log(x-=3);
+console.log(x*=2);
+console.log(x/=4);
+
+console.log("--OPERADORES DE COMPARAÇÃO--");
+
+console.log(4==5);
+console.log(3=='3');
+console.log(undefined==null);
+
+console.log(3==='3');
+
+console.log(null===null);
+console.log(undefined===null);
+
+let obj1= {'nome': 'Rafael'}
+let obj2= {'nome': 'Rafael'}
+
+console.log(obj1===obj2);
+
+console.log(4>3);
+console.log(1>7);
+console.log(2>=2);
+
+console.log(4<3);
+console.log(2<7);
+console.log(3<=9);
